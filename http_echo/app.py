cfd7093accc7c5685560434978e4b87aac03ca9b
Thúlio Costa <thulio@thuliocosta.com>
2018-03-13 21:59:15 -0300

    feat: Capture more request data
    tks @robsonpeixoto

--- http_echo/app.py
+++ http_echo/app.py
@@ -5,21 +5,42 @@ from flask_pymongo import PyMongo
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/http_echo")
 
-DEFAULT_METHODS = ["GET", "POST"]
+ALL_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 
 
-@app.route("/echo", methods=DEFAULT_METHODS)
-def echo():
+@app.route('/', defaults={'path': ''}, methods=ALL_METHODS)
+@app.route('/<path:path>', methods=ALL_METHODS)
+def echo(path):
     args = request.args or {}
-    body = request.get_json() or {}
+    form = request.form
+    json_ = request.get_json()
+    method = request.method
+    headers = dict(request.headers)
+    data = request.data.decode("utf-8") if not json_ else None
+    remote = dict(
+        address=request.environ["REMOTE_ADDR"],
+        port=request.environ["REMOTE_PORT"])
+    content_type = request.content_type
 
-    mongo.db.requests.insert({"args": args, "body": body})
+    mongo_payload = dict(
+        args=args,
+        form=form,
+        json=json_,
+        data=data,
+        method=method,
+        path=path,
+        headers=headers,
+        remote=remote,
+        content_type=content_type)
 
-    return jsonify(dict(ok=True, args=args, body=body))
+    mongo.db.requests.insert(mongo_payload)
+    mongo_payload.pop("_id")
+
+    return jsonify(dict(ok=True, **mongo_payload))
 
 
 if __name__ == "__main__":
